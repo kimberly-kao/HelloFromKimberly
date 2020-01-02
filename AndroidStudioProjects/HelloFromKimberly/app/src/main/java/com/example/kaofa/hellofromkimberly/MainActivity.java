992f5c9f6c8c5bb3236b56fd42938dc9cac2eaa4
kimberly-kao <38386920+kimberly-kao@users.noreply.github.com>
2020-01-02 14:21:37 -0800

    finished pre-work; added button and custom background color

--- AndroidStudioProjects/HelloFromKimberly/app/src/main/java/com/example/kaofa/hellofromkimberly/MainActivity.java
+++ AndroidStudioProjects/HelloFromKimberly/app/src/main/java/com/example/kaofa/hellofromkimberly/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.kaofa.hellofromkimberly;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((TextView) findViewById(R.id.textView3)).setTextColor(getResources().getColor(R.color.colorPrimary));            }
+        });
     }
+
 }
