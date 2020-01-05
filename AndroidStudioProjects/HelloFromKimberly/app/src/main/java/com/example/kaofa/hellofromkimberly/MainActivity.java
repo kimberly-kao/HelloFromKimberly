53bc48b55d8bc5475a9047270415999e5973d20b
kimberly-kao <38386920+kimberly-kao@users.noreply.github.com>
2020-01-05 10:57:41 -0800

    Added a change text button that will change the text to whatever the user inputs unless it is empty, in which it will maintain the same default text. When the background is clicked, the app will be reset to the original text and colors.

--- AndroidStudioProjects/HelloFromKimberly/app/src/main/java/com/example/kaofa/hellofromkimberly/MainActivity.java
+++ AndroidStudioProjects/HelloFromKimberly/app/src/main/java/com/example/kaofa/hellofromkimberly/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.kaofa.hellofromkimberly;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +25,25 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.rootView).setBackgroundColor(getResources().getColor(R.color.pink));
             }
         });
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String newText = ((EditText)findViewById(R.id.editText)).getText().toString();
+                if(TextUtils.isEmpty(newText)){
+                    ((TextView)findViewById(R.id.textView3)).setText("Hello From Kimberly!");
+                }
+                else{
+                    ((TextView)findViewById(R.id.textView3)).setText(newText);
+                }
+            }
+        });
+        findViewById(R.id.rootView).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                findViewById(R.id.rootView).setBackgroundColor(getResources().getColor(R.color.theOPBlueeee));
+                ((TextView)findViewById(R.id.textView3)).setText("Hello From Kimberly!");
+                ((TextView)findViewById(R.id.textView3)).setTextColor(getResources().getColor(R.color.blue));
+            }
+        });
     }
-
 }
